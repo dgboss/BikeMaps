30ba5de7cb6c80d6e05636af1318c32502c0a577
Taylor Denouden <tayden123@hotmail.com>
2014-12-10 13:38:03 -0800

    pagination works and buttons are hidden when they lead to a blank page

--- blog/views.py
+++ blog/views.py
@@ -9,14 +9,21 @@ from blog.models import BlogPost, Category
 
 def index(request, page=0):
 	page = int(page)
-	index = page * 5
+	index = page * 5 #5 is blog posts per page
 	offset = index + 5
 
+	allPosts = BlogPost.objects.all()
+
 	context = {
-		'posts': BlogPost.objects.all()[index:offset],
+		'posts': allPosts[index:offset],
 		'categories': Category.objects.all(),
+
+		'next_page_exists': allPosts[offset:].exists(),
+		'prev_page_exists': page > 0,
+
 		'next_page': page+1,
 		'prev_page': page-1,
+		'curr_page': page,
 
 	}
 
